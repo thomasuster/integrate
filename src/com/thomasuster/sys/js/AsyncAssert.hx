80ddba7c0cea08a48033ed1964ff0cd68c1f99b4
Thomas Uster <thomasuster@gmail.com>
2017-01-13 19:48:58 -0800

    Refac

---
+++ src/com/thomasuster/sys/js/AsyncAssert.hx
@@ -0,0 +1,11 @@
+package com.thomasuster.sys.js;
+import haxe.Timer;
+import massive.munit.async.AsyncFactory;
+class AsyncAssert {
+
+    public static function register(context:Dynamic, factory:AsyncFactory, complete:Void->Void, wait:Int) {
+        var handler:Dynamic = factory.createHandler(context, complete, wait+100);
+        var timer:Timer = Timer.delay(handler, wait);
+    }
+}
+   
