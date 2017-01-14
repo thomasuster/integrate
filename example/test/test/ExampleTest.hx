074704bc840acbe24a9d955215d7ecc09d8dd85e
Thomas Uster <thomasuster@gmail.com>
2017-01-13 19:48:52 -0800

    Refac

--- example/test/test/ExampleTest.hx
+++ example/test/test/ExampleTest.hx
@@ -1,6 +1,6 @@
 package ;
+import com.thomasuster.sys.js.AsyncAssert;
 import com.thomasuster.sys.js.SysClient;
-import haxe.Timer;
 import massive.munit.async.AsyncFactory;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
@@ -10,19 +10,22 @@ class ExampleTest {
     var client:ExampleClient;
     var sys:SysClient;
     
-    @AsyncTest
-    public function testExample(factory:AsyncFactory):Void {
-        var handler:Dynamic = factory.createHandler(this, onTestAsyncExampleComplete, 300);
-        var timer:Timer = Timer.delay(handler, 200);
-        
+    @BeforeClass
+    public function beforeClass():Void {
         sys = new SysClient();
         sys.connect();
+    }
+    
+    @AsyncTest
+    public function testExample(asyncFactory:AsyncFactory):Void {
+        AsyncAssert.register(this, asyncFactory, testExampleDone, 300);
+        
         sys.send('neko', ['../example/server/Build.n']);
         
         client = new ExampleClient();
     }
     
-    private function onTestAsyncExampleComplete():Void  {
+    private function testExampleDone():Void  {
         MatcherAssert.assertThat(client.fromServer, IsEqual.equalTo('pong'));
     }
 }
