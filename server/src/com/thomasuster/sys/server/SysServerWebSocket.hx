ab21d4303f49b20ecbcd84e8d7172d13289f0a2d
Thomas Uster <thomasuster@gmail.com>
2017-01-13 18:30:56 -0800

    Refac

--- server/src/com/thomasuster/sys/server/SysServerWebSocket.hx
+++ server/src/com/thomasuster/sys/server/SysServerWebSocket.hx
@@ -9,21 +9,23 @@ class SysServerWebSocket extends WebSocket {
         try {
             var all:Array<String> = raw.split(',');
             var command:String = all.shift();
-            trace(command);
-            if(command == 'runFrom') {
-                var last:String = Sys.getCwd();
-                runProc('pwd',[]);
-                Sys.setCwd(all[0]);
-                command = all.shift();
-                runProc(command, all);
-                Sys.setCwd(last);
-            }
+            if(command == 'runFrom')
+                runFrom(command, all);
             else
                 runProc(command, all);
         }
         catch(e:Dynamic) {
             print(e);
         }
+    }
+
+    function runFrom(command:String, all:Array<String>):Void {
+        var last:String = Sys.getCwd();
+        runProc('pwd',[]);
+        Sys.setCwd(all[0]);
+        command = all.shift();
+        runProc(command, all);
+        Sys.setCwd(last);
     }
 
     function runProc(command:String, all:Array<String>):Void {
